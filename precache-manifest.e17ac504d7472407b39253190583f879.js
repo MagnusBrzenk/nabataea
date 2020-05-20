8289aa7c74844d0d01e777f5a7603185a94ccf58
MagnusBrzenk <magnusbrzenk@gmail.com>
2020-05-20 14:40:30 -0400

    Updates

---
+++ precache-manifest.e17ac504d7472407b39253190583f879.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71ea539266ab15f73b2e92ff33fd243e",
+    "url": "/nabataea/index.html"
+  },
+  {
+    "revision": "acbb504158d00b9caaef",
+    "url": "/nabataea/static/css/main.451795de.chunk.css"
+  },
+  {
+    "revision": "5570751b177ba4e8fd10",
+    "url": "/nabataea/static/js/2.3b35bfa4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/nabataea/static/js/2.3b35bfa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acbb504158d00b9caaef",
+    "url": "/nabataea/static/js/main.04276ffa.chunk.js"
+  },
+  {
+    "revision": "15b06645a8ed806a0128",
+    "url": "/nabataea/static/js/runtime-main.9f56a6a3.js"
+  }
+]);
