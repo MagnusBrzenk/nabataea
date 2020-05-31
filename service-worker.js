42ed6ee510c2d55ae116d9914adc393a56a4ada1
MagnusBrzenk <magnusbrzenk@gmail.com>
2020-05-30 22:38:13 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nabataea/precache-manifest.397e85bfe14f3db6511ebc69e574a468.js"
+  "/nabataea/precache-manifest.9e7b10602dfddc1622d628bc7b102d41.js"
 );
 
 self.addEventListener('message', (event) => {
